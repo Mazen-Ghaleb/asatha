1ceeb1f82c859183bced51c7b71d84b6b79a70c0
Mazen9 <mmzn971@gmail.com>
2021-09-09 16:40:55 +0200

    Fixed the git commit

---
+++ Frontend/src/components/Forgotten.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+
+export default class Login extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Forgtten Password</h3>
+
+                <div className="form-group">
+                    <label>Email Address</label>
+                    <input type="email" className="form-control" placeholder="Email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Confirm Email Address</label>
+                    <input type="email" className="form-control" placeholder="Confirm Email" />
+                </div>
+
+                <br/>
+
+                <button type="submit" className="btn btn-primary btn-block">Send Reset Request</button>
+            </form>
+        );
+    }
+}
