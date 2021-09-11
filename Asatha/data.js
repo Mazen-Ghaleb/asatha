7459f8c8a8038e0cde4d9b65748fec55ea51bab9
Mazen9 <mmzn971@gmail.com>
2021-09-11 13:41:05 +0200

    Removed unneeded routes and controllers from backend , and added api data file to backend

---
+++ Asatha/data.js
@@ -0,0 +1,16 @@
+const data = [
+    {
+        name: "Mazen",
+        id : "18p"
+    },
+    {
+        name: "Mark",
+        id : "19p"
+    },
+    {
+        name: "Ahmed",
+        id : "20p"
+    },
+];
+
+module.exports = data;
--- Asatha/index.js
+++ Asatha/index.js
@@ -2,16 +2,10 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const homeRoutes = require('./routes/homeRoutes');
-const accountRoutes = require('./routes/accountRoutes');
-const cartRoutes = require('./routes/cartRoutes');
-const productRoutes = require('./routes/productRoutes');
+const data = require("./data");
 
 // creating express App
 const app = express();
-
-// register view engine
-//app.set('view engine', 'ejs');
 
 // Setting server for listening 
 app.listen(process.env.PORT || 5000);
@@ -23,29 +17,5 @@ app.use(morgan('dev'));
 app.use(cors());
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.json(data);
 });
-
-app.get('/', (req, res) => {
-    res.redirect('/Home')
-  });
-
-// to use the Home routes
-app.use('/Home', homeRoutes);
-
-// to use the product routes
-app.use('/Product/:id', productRoutes);
-
-// to use the cart routes
-app.use('/Cart', cartRoutes);
-
-// to use the account routes
-app.use('/Account', accountRoutes);
-
-// for 404 errors
-app.use((request, response)=>{
-  response.write('ERROR YOU PUT WRONG URL!');
-  response.send();
-});
-
-
