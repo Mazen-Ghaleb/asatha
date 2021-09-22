913c2758f9c897ce1178395e6cfe3d9d8e5de95e
Markeag <mark.eaghali@gmail.com>
2021-09-22 08:14:47 +0200

    minor changes to styling of product popup

--- Asatha/Frontend/src/components/bestsellers.js
+++ Asatha/Frontend/src/components/bestsellers.js
@@ -10,19 +10,19 @@ export default[
         img: productimg1,
         title:'Apple IMac (2019) 21.5" ',
         description:'idfk',
-        price:'21,000 LE',
+        price:'21,000 ',
     },{
         brand:'Lenovo',
         img: productimg2,
         title:'Lenovo Legion 5 Gaming Laptop 15.6" ',
         description:' Lenovo Legion 5 15ARH05H Gaming Laptop 15.6" 120Hz IPS FHD, AMD Ryzen 5 4600H, GTX 1650Ti 4GB GPU',
-        price:'15,000 LE',
+        price:'15,000 ',
     },{
         brand:'Evga',
         img: productimg3,
         title:'Brand New EVGA GeForce RTX 3060 Ti  ',
         description:'Brand New EVGA GeForce RTX 3060 Ti XC 8GB GDDR6 Gaming Video Card',
-        price:'13,500 LE',
+        price:'13,500 ',
         link:'/Product'
 
     },
--- Asatha/Frontend/src/components/recentpurchases.js
+++ Asatha/Frontend/src/components/recentpurchases.js
@@ -9,20 +9,20 @@ export default[
         category:'Apple',
         img: productimg1,
         title:'Apple IMac (2019) 21.5" ',
-        description:'idfk',
-        price:'21,000 LE',
+        description:'A desktop experience that draws you in and keeps you there. This is the idea behind today’s iMac. And now that idea is more powerful than ever. The new iMac is packed with all-new processors, the latest graphics technologies, innovative storage, and higher-bandwidth connectivity. And it all comes to life on the brightest and most colorful Retina display iMac has ever seen. So you get an even more immersive experience.',
+        price:'21,000 ',
     },{
         brand:'Lenovo',
         img: productimg2,
         title:'Lenovo Legion 5 Gaming Laptop 15.6" ',
         description:' Lenovo Legion 5 15ARH05H Gaming Laptop 15.6" 120Hz IPS FHD, AMD Ryzen 5 4600H, GTX 1650Ti 4GB GPU',
-        price:'15,000 LE',
+        price:'15,000 ',
     },{
         brand:'Evga',
         img: productimg3,
         title:'Brand New EVGA GeForce RTX 3060 Ti  ',
         description:'Brand New EVGA GeForce RTX 3060 Ti XC 8GB GDDR6 Gaming Video Card',
-        price:'13,500 LE',
+        price:'13,500 ',
         link:'/Product'
 
     },
