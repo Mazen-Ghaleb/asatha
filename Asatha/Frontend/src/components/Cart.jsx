79598c78bd085b790352ad1f12005b4cbafb3a72
Markeag <mark.eaghali@gmail.com>
2021-09-22 08:03:18 +0200

    restored legacy cart

--- Asatha/Frontend/src/components/Cart.jsx
+++ Asatha/Frontend/src/components/Cart.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import productimg1 from '../images/product-1.jpg';
+import productimg1 from '../images/product-1.jpg'
 
 function getProductInfos(props) {
   if (props) {
@@ -29,57 +29,64 @@ function getOrderItemInfos(props) {
 export default class Cart extends Component {
   render() {
     return (
+
+
       <html class="cart-html">
-        {/* <div>
-          <pre>{getProductInfos(this.props)}</pre>
-        </div> */}
-        <div>
-          <header id="cart-site-header">
+          {/* <div>
+            <pre>{getProductInfos(this.props)}</pre>
+          </div> */}
+          <div>
+            <header id="cart-site-header">
+              <div className="cart-container">
+                <h1 class="cart-header">Shopping cart</h1>
+              </div>
+            </header>
             <div className="cart-container">
-              <h1 class="cart-header">Shopping cart</h1>
+              <section id="cart">
+                <article className="cart-product">
+                  <header>
+                    <img className="cart-medium" src={productimg1} alt="" />
+                    <h3 className="sh"><button className="rmvb">Remove product</button></h3>
+                  </header>
+                  <div className="cart-content">
+                    <h1>item one</h1>
+                    <p>some description goes here</p>
+                    <div title="You have selected this product"></div>
+                  </div>
+                  <div className="crtbtns">
+                    <button className="abtn">+</button>
+                    <div className="amnt">2</div>
+                    <button className="abtn">-</button>
+                  </div>
+                </article>
+              </section> 
             </div>
-          </header>
-          <div className="cart-container">
-            <section id="cart">
-              <article className="cart-product">
-                <header>
-                  <img className="cart-medium" src={productimg1} alt="" />
-                  <h3 className="sh"><button className="rmvb">Remove product</button></h3>
-                </header>
-                <div className="cart-content">
-                  <h1>item one</h1>
-                  <p>some description goes here</p>
-                  <div title="You have selected this product"></div>
+            <footer id="cart-site-footer">
+              <div class="cart-container clearfix">
+                <div class="cart-left">
+                  <h2 class="cart-subtotal">
+                    Subtotal: <span>sub total in numbers</span>€
+                  </h2>
+                  <h3 class="cart-tax">
+                    Taxes (5%): <span>tax cost in numbers</span>€
+                  </h3>
+                  <h3 class="cart-shipping">
+                    Shipping: <span>5.00</span>€
+                  </h3>
                 </div>
-                
-              </article>
-            </section> 
+                <div class="cart-right">
+                  <h1 class="cart-total">
+                    Total: <span>total price</span>€
+                  </h1>
+                  <button type="cart-submit" className="cart-btn">
+                    Checkout
+                  </button>
+                </div>
+              </div>
+            </footer>
           </div>
-          <footer id="cart-site-footer">
-            <div class="cart-container clearfix">
-              <div class="cart-left">
-                <h2 class="cart-subtotal">
-                  Subtotal: <span>sub total in numbers</span>€
-                </h2>
-                <h3 class="cart-tax">
-                  Taxes (5%): <span>tax cost in numbers</span>€
-                </h3>
-                <h3 class="cart-shipping">
-                  Shipping: <span>5.00</span>€
-                </h3>
-              </div>
-              <div class="cart-right">
-                <h1 class="cart-total">
-                  Total: <span>total price</span>€
-                </h1>
-                <button type="cart-submit" className="cart-btn">
-                  Checkout
-                </button>
-              </div>
-            </div>
-          </footer>
-        </div>
-      </html>
+        </html>
+
     );
   }
 }
